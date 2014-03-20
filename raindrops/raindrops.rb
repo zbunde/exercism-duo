bd711b6e25c6fabf73eef7741fe8d6a65f86d4a7
Zach Klabunde <zbunde@gmail.com>
2014-03-20 08:45:55 -0600

    with map

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,28 +1,11 @@
 class Raindrops
   def self.convert(num)
-    number = num.to_i
-    string = ''
 
-    if number % 3 == 0
+  {3 => "Pling", 5 => "Plang", 7 => "Plong"}.map  do |divisor, word|
+    word if num % divisor == 0
 
-      string += 'Pling'
-    end
+  end.compact.tap {|whatever| whatever << num if whatever.empty?}.join
 
-    if number % 5 ==0
-      string +='Plang'
-    end
-
-    if number % 7 ==0
-      string +='Plong'
-    end
-
-    if number %3 != 0 && number %5 != 0 && number %7 != 0
-
-      string = number.to_s
-
-    end
-
-    string
 
   end
 end
